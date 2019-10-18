3506a882294e02efab54d8e3fc6acd32ce2d7fa0
Preveen Raj <97preveenraj@gmail.com>
2019-10-18 22:37:53 +0530

    rest-api

--- monolithic-services/truyum/src/main/java/com/cognizant/truyum/exception/CartEmptyException.java
+++ monolithic-services/truyum/src/main/java/com/cognizant/truyum/exception/CartEmptyException.java
@@ -2,6 +2,6 @@ package com.cognizant.truyum.exception;
 
 public class CartEmptyException extends Exception {
 	public CartEmptyException() {
-		super("\nCart is Empty");
+		super("Cart is Empty");
 	}
 }
